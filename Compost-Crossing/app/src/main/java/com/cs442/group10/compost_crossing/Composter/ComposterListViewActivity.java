07b7ef9a8d3265793f8c8d98178706a48f3f97e9
Harshavardhan Patil <HarshPatil@Harshavardhans-MacBook-Pro.local>
2016-11-10 16:33:50 -0600

    implemented patch 9

--- Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/Composter/ComposterListViewActivity.java
+++ Compost-Crossing/app/src/main/java/com/cs442/group10/compost_crossing/Composter/ComposterListViewActivity.java
@@ -1,25 +1,123 @@
 package com.cs442.group10.compost_crossing.Composter;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.cs442.group10.compost_crossing.MainActivity;
 import com.cs442.group10.compost_crossing.R;
+import com.cs442.group10.compost_crossing.constants.Constants;
+import com.cs442.group10.compost_crossing.newsArticle.Article;
+import com.cs442.group10.compost_crossing.preferences.MyPreferenceActivity;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.SimpleShowcaseEventListener;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 public class ComposterListViewActivity extends AppCompatActivity {
+
+    private ListView mDrawerList;
+    private String[] drawerList;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private static final int SHOW_PREFERENCES = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composter_list_view);
 
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer_module_list);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_module_list);
+
+        drawerList = new String[3];
+        drawerList[0] = Constants.HOME;
+        drawerList[1] = Constants.NEWS_ARTICLE;
+        drawerList[2] = Constants.SETTINGS;
+
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.navigation_list_item, drawerList));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_audiotrack, R.string.drawer_open, R.string.drawer_close) {
+
+            public void onDrawerClosed(View view) {
+
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+//        SharedPreferences sharedPreferences = getSharedPreferences("com.cs442.group10.compost_crossing.Composter.ComposterListViewActivity", MODE_PRIVATE);
+//        boolean isFirstRun = sharedPreferences.getBoolean("composterFirstrun", true);
+//        if (isFirstRun) {
+//
+//            sharedPreferences.edit().putBoolean("composterFirstrun", false).commit();
+//            showFirstShowCase();
+//        }
     }
 
     public void onBackCompostListView(View view){
         Intent intent = new Intent(this.getApplicationContext(), MainActivity.class);
         startActivity(intent);
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+
+        if(position==0){
+
+            Intent intent=new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        } else if(position==1){
+
+            Intent intent=new Intent(this, Article.class);
+            startActivity(intent);
+
+        } else if(position == 2){
+
+            Class<?> c = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB ? MyPreferenceActivity.class:MyPreferenceActivity.class;
+            Intent i = new Intent(this, c);
+            startActivityForResult(i, SHOW_PREFERENCES);
+
+        }
+    }
+
+//    private void showFirstShowCase(){
+//        new ShowcaseView.Builder(this)
+//                .withMaterialShowcase()
+//                .setStyle(R.style.CustomShowcaseTheme2)
+//                .setTarget(new ViewTarget(back))
+//                .hideOnTouchOutside()
+//                .setContentTitle("Click the button if you are a composter.")
+//                .setShowcaseEventListener(new SimpleShowcaseEventListener() {
+//
+//                    @Override
+//                    public void onShowcaseViewDidHide(ShowcaseView showcaseView) {
+////                        showSecondShowCase();
+//                    }
+//
+//                })
+//                .build();
+//    }
 }
